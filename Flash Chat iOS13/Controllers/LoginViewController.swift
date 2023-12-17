1e4d1c2f65c4cd4141ba2b8a004b2dcf450e891d
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-17 15:20:11 +0600

    SignIN Added

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
+
 
 class LoginViewController: UIViewController {
 
@@ -15,6 +18,17 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text , let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let err = error {
+                    print(err.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self )
+                }
+              
+            }
+        }
     }
     
 }
