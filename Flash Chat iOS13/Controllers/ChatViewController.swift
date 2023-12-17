99d0d126636a92696eba0f2cb8e1c7210c32183a
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-17 15:32:11 +0600

    LogOut Added

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
@@ -22,4 +23,14 @@ class ChatViewController: UIViewController {
     }
     
 
+    @IBAction func logoutPressed(_ sender: UIBarButtonItem) {
+        
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+    }
 }
