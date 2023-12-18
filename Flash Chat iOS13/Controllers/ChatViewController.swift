27c27dff077cfb79e213b7e39f43f121eefd4610
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-18 14:00:26 +0600

    fireStoreRulesAdded

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,12 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
-    var db = Firestore.firestore()
     
+    var db = Firestore.firestore()
     var chatHistory: [Messages] = []
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         tableView.dataSource = self
@@ -28,19 +29,29 @@ class ChatViewController: UIViewController {
         navigationItem.hidesBackButton = true 
         
         tableView.register( UINib(nibName: K.cellNibName , bundle: nil ), forCellReuseIdentifier: K.cellIdentifier )
-        
         loadMessages();
 
     }
     
     func loadMessages() {
-        db.collection(K.FStore.collectionName).getDocuments() { (querySnapshot, err) in
+        
+        /*
+         db.collection(K.FStore.collectionName).getDocuments() { (querySnapshot, err) in
+         getDocumnets is used for reading Documents from fireStore but it doesn't response to update.
+         */
+        
+        db.collection(K.FStore.collectionName).order(by: K.FStore.dateField)
+            .addSnapshotListener() { (querySnapshot, err) in
           if let err = err {
             print("Error getting documents: \(err)")
           } else {
+              
+              self.chatHistory = []
+              
               if let snapDocs =  querySnapshot?.documents {
                   
                   for docs in snapDocs {
+                      print(docs.documentID)
                       let data = docs.data()
                       if let sender = data[K.FStore.senderField] as? String ,  let body = data[K.FStore.bodyField] as? String {
                           self.chatHistory.append(Messages(sender: sender, body: body))
@@ -64,12 +75,13 @@ class ChatViewController: UIViewController {
     
     @IBAction func sendPressed(_ sender: UIButton) {
         
-
+        
         if let messageBody = messageTextfield.text , let messageSender = Auth.auth().currentUser?.email {
             
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField : messageBody
+                K.FStore.bodyField : messageBody,
+                K.FStore.dateField : Date().timeIntervalSince1970
             ]) { err in
               if let err = err {
                 print("Error adding document: \(err)")
@@ -80,6 +92,8 @@ class ChatViewController: UIViewController {
             
             
         }
+        
+        messageTextfield.text = "" 
 
     }
     
